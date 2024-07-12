0ae2772936f5f28c6d7fa4a4574b2508e2790df3
Vivek Vijayan <vivekvijayan6282@gmail.com>
2024-07-12 17:30:00 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+setup(
+    name="everest",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=['numpy', 'tensorflow'],
+    classifiers=[
+        "Development Status :: 1 - Production",
+        "Intended Audience :: Developers, Researchers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
